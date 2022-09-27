d62ee90fba2b2bb6d86f75eae74884f961d5d2b5
wbel4 <83471418+wbel4@users.noreply.github.com>
2022-09-27 17:36:05 -0400

    Studio 3 Done

--- app.js
+++ app.js
@@ -26,15 +26,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const fruityArray = fruitsArray.map((item) => {
+    return item.name;
+  })
+
 
   // TODO: Create a new HTML element to display your data
-
+  const newElement = document.createElement('div');
+  newElement.innerHTML = fruityArray;
   // TODO: Append your new element to the page
-
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
